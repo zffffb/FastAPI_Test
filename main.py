4825b5b6a1ef2eae5b1d889b4437151e6d2f95c5
Ethan <5948120@qq.com>
2025-03-27 22:09:53 +0800

    change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from apps.user import user
 
 app = FastAPI()
 
-app.include_router(app_user,tags = ["user"])
+app.include_router(user,tags = ["user"])
 
 if __name__ == "__main__":
     uvicorn.run("main :app",reload = 8000) 
